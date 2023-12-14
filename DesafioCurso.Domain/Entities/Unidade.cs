03527d37e37c5a53631a683bc46bd839a54abfa3
Jose Rodrigo <rodrigovargasv80@gmail.com>
2023-12-14 13:40:57 -0300

    Criado a classe Unidade com suas propriedades basicas.

---
+++ DesafioCurso.Domain/Entities/Unidade.cs
@@ -0,0 +1,12 @@
+﻿
+using DesafioCurso.Domain.Commons;
+
+namespace DesafioCurso.Domain.Entities
+{
+    public class Unidade : EntityBase
+    {
+        public string Sigla { get; set; }
+
+        public string Descricao { get; set; }
+    }
+}
