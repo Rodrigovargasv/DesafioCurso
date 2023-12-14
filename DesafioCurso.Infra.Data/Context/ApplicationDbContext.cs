dcf3f8039b1aad6a094f564d285b46c0c44e655d
Jose Rodrigo <rodrigovargasv80@gmail.com>
2023-12-14 15:10:48 -0300

    Importado as bibliotecas básicas para a realização da conexão com banco de dados, mapeamento das entidades e modelagem de dados.
    Por fim, foi Implementando os métodos para configuração do contexto do banco de dados, mapeamento e aplicação automática de configurações de entidades.

---
+++ DesafioCurso.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿
+using DesafioCurso.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesafioCurso.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Configura contexto do banco de dados
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContext) : base(dbContext) { }
+
+
+        // Configurar o mapeamento das entidades para o banco de dados
+        public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Unidade> Unidades { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+
+        // Realiza a aplicação automatica das configurações de entidades que são definidas nas pasta EntityConfiguration
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+    }
+}
