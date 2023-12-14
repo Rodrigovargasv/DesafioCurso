6c8cb56798124dda8f75945d87451891428d6135
Jose Rodrigo <rodrigovargasv80@gmail.com>
2023-12-14 14:52:52 -0300

    Criado a classe Usuario com suas propriedades básicas.

---
+++ DesafioCurso.Domain/Entities/Usuario.cs
@@ -0,0 +1,14 @@
+﻿
+using DesafioCurso.Domain.Commons;
+
+namespace DesafioCurso.Domain.Entities
+{
+    public class Usuario : EntityBase
+    {
+        public string NomeCompleto { get; set; }
+        public string Apelido { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public int CpfCnpj { get; set; }
+    }
+}
