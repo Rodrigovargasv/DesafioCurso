f3d9f2cc081b745e09b66a8a83a6ca7045fc1a9e
Jose Rodrigo <rodrigovargasv80@gmail.com>
2023-12-14 14:42:59 -0300

    Criado a classe Produto com suas propriedades básicas.

---
+++ DesafioCurso.Domain/Entities/Produto.cs
@@ -0,0 +1,17 @@
+﻿
+using DesafioCurso.Domain.Commons;
+
+namespace DesafioCurso.Domain.Entities
+{
+    public class Produto : EntityBase
+    {
+        public string DescricaoCompleta { get; set; }
+        public string DescricaoResumida { get; set; }
+        public decimal Preco { get; set; } 
+        public int QuantidadeEstoque { get; set;}
+        public int CodigoBarras { get; set; }
+        public bool Ativo { get; set;}
+
+        public Unidade unidade { get; set;}
+    }
+}
