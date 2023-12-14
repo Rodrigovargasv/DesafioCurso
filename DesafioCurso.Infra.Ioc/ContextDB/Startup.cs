fca5346429c5caac3fdcee49f032034b6a7ec2a3
Jose Rodrigo <rodrigovargasv80@gmail.com>
2023-12-14 17:56:08 -0300

    Configurado o serviço de injeção de dependência do banco de dados.

---
+++ DesafioCurso.Infra.Ioc/ContextDB/Startup.cs
@@ -0,0 +1,23 @@
+﻿
+using DesafioCurso.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DesafioCurso.Infra.Ioc.ContextDB
+{
+    internal static class Startup
+    {
+        internal static IServiceCollection AddServicesDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(opt => opt.UseNpgsql(configuration.GetConnectionString("DefaultConnection"),
+                builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            // Altera o formato de data e hora do banco de dados PostgreSql
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+            return services;
+
+        }
+    }
+}
---
+++ DesafioCurso.Infra.Ioc/StartupBase.cs
@@ -0,0 +1,18 @@
+﻿
+using DesafioCurso.Infra.Ioc.ContextDB;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DesafioCurso.Infra.Ioc
+{
+    public static class StartupBase
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Serviço de banco de dados
+            services.AddServicesDbContext(configuration);
+
+            return services;
+        }
+    }
+}
